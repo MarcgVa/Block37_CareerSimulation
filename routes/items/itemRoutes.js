782cac774dd302d2f9809c1a39e91d23a1aa1312
Alyssa <ashinnickmitchell@gmail.com>
2025-04-25 08:19:26 -0600

    deleted unneeded middleware

--- routes/items/itemRoutes.js
+++ routes/items/itemRoutes.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const { getAllItems, getItem, getItemReviews } = require("./itemsController");
 
-router.get("/items", middleware, getAllItems);
-router.get("/items/:itemId", middleware, getItem);
-router.get("/items/:itemId/reviews", middleware, getItemReviews);
+router.get("/items", getAllItems);
+router.get("/items/:itemId", getItem);
+router.get("/items/:itemId/reviews", getItemReviews);
 
 module.exports = router;
