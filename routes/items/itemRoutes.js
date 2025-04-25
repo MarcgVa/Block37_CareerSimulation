642704749ab811176efee06e4b9c8afd48902e1c
Alyssa <ashinnickmitchell@gmail.com>
2025-04-24 21:18:45 -0600

    imported itemRoutes into index.js

---
+++ routes/items/itemRoutes.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const { getAllItems, getItem, getItemReviews } = require("./itemsController");
+
+router.get("/items", middleware, getAllItems);
+router.get("/items/:itemId", middleware, getItem);
+router.get("/items/:itemId/reviews", middleware, getItemReviews);
+
+module.exports = router;
---
+++ routes/items/itemsController.js
@@ -0,0 +1,32 @@
+const { prisma } = require("../../common/common");
+
+const getAllItems = async (req, res) => {
+  try {
+    const items = await prisma.item.findMany();
+    res.json(items);
+  } catch (error) {
+    res.status(500).send("Error on server side");
+  }
+};
+
+const getItem = async (req, res) => {
+  const { itemId } = req.params;
+  try {
+    const item = await prisma.item.findUnique({
+      where: {
+        id,
+      },
+    });
+    res.json(item);
+  } catch (error) {
+    res.status(500).send("Error on server side");
+  }
+};
+
+const getItemReviews = async (req, res) => {};
+
+module.exports = {
+  getAllItems,
+  getItem,
+  getItemReviews,
+};
